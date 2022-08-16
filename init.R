0420ea97f793cbc43f1ac315de3e4bfcdebba743
adrianstando <adrsnek11@live.com>
2022-08-16 16:43:13 +0200

    Add file for heroku

---
+++ init.R
@@ -0,0 +1,9 @@
+# install libraries for heroku
+
+my_packages <- c('shiny', 'DT', 'tidyr', 'iBreakDown', 'ggplot2', 'sortable', 'stringr', 'dplyr', 'tidyr')
+install_if_missing = function(p) {
+  if (p %in% rownames(installed.packages()) == FALSE) {
+    install.packages(p)
+  }
+}
+invisible(sapply(my_packages, install_if_missing)) 
--- run.R
+++ run.R
@@ -1,8 +1,3 @@
-# install packages for heroku
-list.of.packages <- c('shiny', 'DT', 'tidyr', 'iBreakDown', 'ggplot2', 'sortable', 'stringr', 'dplyr', 'tidyr')
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
-
 library(shiny)
 
 port <- Sys.getenv('PORT')
